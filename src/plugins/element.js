2c706e0966bab668c10b51c93accc6d800023493
NgChiKin <ng_chikin@163.com>
2019-11-26 18:32:18 +0800

    更新登录界面

---
+++ src/plugins/element.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import {
+  Button,
+  Input,
+  Form,
+  FormItem,
+  Row,
+  Col,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  RadioGroup,
+  RadioButton,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination
+}
+  from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.prototype.$message = Message
