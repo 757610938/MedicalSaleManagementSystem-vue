093ec4812250b8df593ec578607f6a1ea9176e02
757610938 <757610938@qq.com>
2019-12-17 01:32:26 +0800

    修改采购界面

--- src/components/purchase/Porder.vue
+++ src/components/purchase/Porder.vue
@@ -13,6 +13,9 @@
         <el-form-item label="建单日期:" prop="purDate" class="from-inlines">
           <el-input v-model="purFrom.purDate" clearable></el-input>
         </el-form-item>
+        <el-form-item label="采购单备注:" prop="purRemark" class="from-inlines">
+          <el-input v-model="purFrom.purRemark"></el-input>
+        </el-form-item>
       </el-form>
       <el-form :inline="true" :model="purFrom">
         <el-form-item label="采购单名称:" prop="purName" class="from-inline">
@@ -22,33 +25,38 @@
           <el-input v-model="purFrom.supplierId" clearable></el-input>
         </el-form-item>
         <el-form-item label="总金额:" prop="purTotalMoney" class="from-inlines" label-width="70px">
-          <el-input v-model="purFrom.purTotalMoney" clearable></el-input>
-        </el-form-item>
-      </el-form>
-      <el-form :inline="true" :model="purFrom">
-        <el-form-item label="采购单状态:" prop="purStatus" class="from-inlines">
-          <el-input v-model="purFrom.purStatus"></el-input>
-        </el-form-item>
-        <el-form-item label="采购单备注:" prop="purRemark" class="from-inlines">
-          <el-input v-model="purFrom.purRemark"></el-input>
+          <el-input v-model="purFrom.purTotalMoney" clearable disabled></el-input>
         </el-form-item>
       </el-form>
       <el-button type="text" class="from-inlines" @click="medicineDialog">添加药品</el-button>
       <el-table :data="purFrom.purDtlList" border stripe :row-key="purFrom.purDtlList.medicineId">
         <el-table-column label="药品编号" prop="medicineId" width="300px"></el-table-column>
-        <el-table-column label="药品名称" prop="medicineName"></el-table-column>
-        <el-table-column label="采购单价" prop="purDtlPrcie"></el-table-column>
-        <el-table-column label="采购数量">
+        <el-table-column label="药品名称" prop="medicineName" width="200px"></el-table-column>
+        <el-table-column label="采购单价" width="200px">
           <template slot-scope="scope">
-            <el-input-number
-              v-model="scope.row.purDtlAmount"
+            <el-input
+              v-model="scope.row.purDtlPrice"
               controls-position="right"
               @change="handleChange"
               :min="1"
-            >{{ scope.row }}</el-input-number>
+            ></el-input>
           </template>
         </el-table-column>
-        <el-table-column label="采购项备注" prop="purDtlRemark" width="500px"></el-table-column>
+        <el-table-column label="采购数量" width="200px">
+          <template slot-scope="scope">
+          <el-input-number
+            v-model="scope.row.purDtlAmount"
+            controls-position="right"
+            @change="handleChange"
+            :min="1"
+          ></el-input-number>
+        </template>
+        </el-table-column>
+        <el-table-column label="采购项备注" width="500px">
+          <template slot-scope="scope">
+            <el-input type="textarea"  v-model="scope.row.purDtlRemark" @change="handleChange" ></el-input>
+          </template>
+        </el-table-column>
         <el-table-column label="操作" width="80px" fixed="right">
           <template slot-scope="scope">
             <el-tooltip effect="dark" content="删除" placement="top" :enterable="false">
@@ -62,10 +70,10 @@
           </template>
         </el-table-column>
       </el-table>
-      <el-button type="primary" class="el-card" @clik="savePur">保存</el-button>
-      <el-button type="primary">提交审核</el-button>
+      <el-button type="primary" class="el-card" @click="savePur">保存</el-button>
+      <el-button type="primary" @click="submitPur">提交审核</el-button>
     </el-card>
-    <el-dialog title="收货地址" :visible.sync="medicineDialogTableVisible">
+    <el-dialog title="添加药品" :visible.sync="medicineDialogTableVisible">
       <el-input placeholder="请输入内容" v-model="queryInfo.name" clearable @clear="getMedicineList">
         <el-button slot="append" icon="el-icon-search" @click="getMedicineList"></el-button>
       </el-input>
@@ -73,9 +81,15 @@
         <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column type="index" fixed label="序号" width="55"></el-table-column>
         <el-table-column label="编号" prop="medicineId" width="150"></el-table-column>
-        <el-table-column label="批准文号" prop="medicineApprovalNumber" width="300"></el-table-column>
         <el-table-column label="药品名" prop="medicineName" width="200"></el-table-column>
         <el-table-column label="药品英文名称" prop="medicineEnglishName" width="200"></el-table-column>
+        <el-table-column label="零售价" prop="medicineRetailPrice" width="150"></el-table-column>
+        <el-table-column label="进货价" prop="medicinePurchasePrice" width="150"></el-table-column>
+        <el-table-column label="批发价" prop="medicineWholesalePrice" width="150"></el-table-column>
+        <el-table-column label="药品库存量" prop="stochAmount" width="150"></el-table-column>
+        <el-table-column label="药品存放仓库" prop="whseName" width="200"></el-table-column>
+        <el-table-column label="药品供应商名" prop="supplierName" width="200"></el-table-column>
+        <el-table-column label="批准文号" prop="medicineApprovalNumber" width="300"></el-table-column>
         <el-table-column label="剂型种类" prop="medicineDosageformCategory" width="150"></el-table-column>
         <el-table-column label="生产单位" prop="medicineManufacturer" width="250"></el-table-column>
         <el-table-column label="生产单位地址" width="500" prop="medicineManufacturingAddress"></el-table-column>
@@ -84,12 +98,6 @@
         <el-table-column label="批准日期" prop="medicineApprovaldate" width="200"></el-table-column>
         <el-table-column label="原批准文号" width="300" prop="medicineOriginalApprovalNumber"></el-table-column>
         <el-table-column label="国产药/进口药" prop="medicineCountry" width="150"></el-table-column>
-        <el-table-column label="进货价" prop="medicinePurchasePrice" width="150"></el-table-column>
-        <el-table-column label="零售价" prop="medicineRetailPrice" width="150"></el-table-column>
-        <el-table-column label="批发价" prop="medicineWholesalePrice" width="150"></el-table-column>
-        <el-table-column label="药品供应商名" prop="supplierName" width="200"></el-table-column>
-        <el-table-column label="药品库存量" prop="stochAmount" width="150"></el-table-column>
-        <el-table-column label="药品存放仓库" prop="whseName" width="200"></el-table-column>
         <el-table-column label="操作" width="70px" fixed="right">
           <template slot-scope="scope">
             <el-tooltip effect="dark" content="添加" placement="top" :enterable="false">
@@ -224,6 +232,10 @@ export default {
     },
     async savePur() {
       const { data: res } = await this.$http.post('purchase', this.purFrom)
+      console.log(res.message)
+    },
+    async submitPur() {
+      const { data: res } = await this.$http.post('purchase/submitPur', this.purFrom)
       console.log(res.message)
     }
   }
